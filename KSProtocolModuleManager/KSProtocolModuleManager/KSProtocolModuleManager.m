dd2e4debbf609e579b79eef9a5cf690def99533a
Ping <pingyandong0510@126.com>
2016-12-26 17:26:15 +0800

    add

--- KSProtocolModuleManager/KSProtocolModuleManager/KSProtocolModuleManager.m
+++ KSProtocolModuleManager/KSProtocolModuleManager/KSProtocolModuleManager.m
@@ -8,6 +8,43 @@
 
 #import "KSProtocolModuleManager.h"
 
+@interface KSProtocolModuleManager ()
+@property (nonatomic, strong) NSMutableDictionary *serviceProvideSource;
+
+@end
+
 @implementation KSProtocolModuleManager
+
++ (KSProtocolModuleManager *)sharedInstance
+{
+    static KSProtocolModuleManager * instance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[self alloc] init];
+    });
+    return instance;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _serviceProvideSource = [[NSMutableDictionary alloc] init];
+    }
+    return self;
+}
+
++ (void)registServiceProvide:(id)provide forProtocol:(Protocol*)protocol
+{
+    if (provide == nil || protocol == nil)
+        return;
+    [[self sharedInstance].serviceProvideSource setObject:provide forKey:NSStringFromProtocol(protocol)];
+}
+
++ (id)serviceProvideForProtocol:(Protocol *)protocol
+{
+    return [[self sharedInstance].serviceProvideSource objectForKey:NSStringFromProtocol(protocol)];
+}
+
 
 @end
